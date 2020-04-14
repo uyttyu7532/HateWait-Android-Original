130adaaa3bcb05cfa03294ce08ee2a405efc957f
choyerin <uyttyu7532@naver.com>
2020-04-15 02:06:19 +0900

    manifest필요없는 코드 삭제/fcm코드 주석처리

--- app/src/main/java/com/example/hatewait/MainActivity.kt
+++ app/src/main/java/com/example/hatewait/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.hatewait
 
-import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
-import java.io.IOException
 
 
 
@@ -18,7 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         var id = "건대보쌈"
         var waitingnum = "10"
@@ -49,23 +43,23 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun myToken() =//쓰레드 사용할것
-        Thread(Runnable {
-            try {
-                FirebaseInstanceId.getInstance().instanceId
-                    .addOnCompleteListener(OnCompleteListener { task ->
-                        if (!task.isSuccessful) {
-                            Log.i(TAG, "getInstanceId failed", task.exception)
-                            return@OnCompleteListener
-                        }
-                        val token = task.result?.token
-//                        textView.text = token
-                        //Log.d(TAG, token)
-                    })
-            } catch (e: IOException){
-                e.printStackTrace()
-            }
-        }).start()
+//    private fun myToken() =//쓰레드 사용할것
+//        Thread(Runnable {
+//            try {
+//                FirebaseInstanceId.getInstance().instanceId
+//                    .addOnCompleteListener(OnCompleteListener { task ->
+//                        if (!task.isSuccessful) {
+//                            Log.i(TAG, "getInstanceId failed", task.exception)
+//                            return@OnCompleteListener
+//                        }
+//                        val token = task.result?.token
+////                        textView.text = token
+//                        //Log.d(TAG, token)
+//                    })
+//            } catch (e: IOException){
+//                e.printStackTrace()
+//            }
+//        }).start()
 
 
 

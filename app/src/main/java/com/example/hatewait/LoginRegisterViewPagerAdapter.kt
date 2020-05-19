be5017d1e47e579fb64bb069c596a12683c4da8a
문승훈 <mbh023@gmail.com>
2020-05-20 02:39:15 +0900

    "ViewPager1 -> 2 update"

--- app/src/main/java/com/example/hatewait/LoginRegisterViewPagerAdapter.kt
+++ app/src/main/java/com/example/hatewait/LoginRegisterViewPagerAdapter.kt
@@ -8,52 +8,41 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_register_tab_pager.*
+import org.jetbrains.anko.support.v4.viewPager
 
 //Recycler View Adpater
 class LoginRegisterViewPagerActivity : AppCompatActivity() {
+    val tabNameArray = arrayOf<String>("비회원", "회원")
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_tab_pager)
 //        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        kind_of_member_tab_layout.addTab(kind_of_member_tab_layout.newTab().setText("비회원"))
-        kind_of_member_tab_layout.addTab(kind_of_member_tab_layout.newTab().setText("회원"))
-
-        val pagerAdapter = PagerAdapter(supportFragmentManager, tabCount = 2)
-        view_pager.adapter = pagerAdapter
-
-//        ViewPager2 버전 사용시.. 하
-//        TabLayoutMediator(kind_of_member_tab_layout, view_pager) {
-//                tab, position ->  Log.d("this position: ", (position + 1).toString())
-//        }.attach()
-        kind_of_member_tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-                override fun onTabSelected(tab: TabLayout.Tab?) {
-                    view_pager.currentItem = tab!!.position
-                }
-                override fun onTabReselected(tab: TabLayout.Tab?) {
-                }
-                override fun onTabUnselected(tab: TabLayout.Tab?) {
-                }
-            })
-
-        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(kind_of_member_tab_layout))
+        init()
 
     }
-}
+    private fun init() {
+        view_pager.adapter = ScreenSlidePagerAdapter(this)
+        TabLayoutMediator(kind_of_member_tab_layout, view_pager) {
+                tab, position ->
+            tab.setText(tabNameArray[position])
+        }.attach()
+        //attach() :    Link the TabLayout and the ViewPager2 together.
+        // Must be called after ViewPager2 has an adapter set
 
+//        kind_of_member_tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//                override fun onTabSelected(tab: TabLayout.Tab?) {
+//                    view_pager.currentItem = tab!!.position
+//                }
+//                override fun onTabReselected(tab: TabLayout.Tab?) {
+//                }
+//                override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                }
+//            })
 
+//        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(kind_of_member_tab_layout))
 
-class PagerAdapter (fragmentManager: FragmentManager, val tabCount: Int) : FragmentStatePagerAdapter(fragmentManager) {
-    override fun getItem(position: Int): Fragment {
-        when (position) {
-            0-> return NonMemberRegister()
-            1-> return MemberRegister()
-            else -> return NonMemberRegister()
-        }
-    }
-
-    override fun getCount(): Int {
-        return tabCount
     }
 }

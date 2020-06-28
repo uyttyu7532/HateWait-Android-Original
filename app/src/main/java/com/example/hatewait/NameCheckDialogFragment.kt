f442bbb248847585f10c9b6027e8b2551e82902d
milkcoke <mbh023@konkuk.ac.kr>
2020-06-28 21:05:56 +0900

    "Dialog Positive/Negative Button align & resize trying~"

--- app/src/main/java/com/example/hatewait/NameCheckDialogFragment.kt
+++ app/src/main/java/com/example/hatewait/NameCheckDialogFragment.kt
@@ -1,17 +1,20 @@
 package com.example.hatewait
 
-import android.app.Activity
 import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
+import android.content.DialogInterface.OnShowListener
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import com.twilio.rest.monitor.v1.Alert
+
 
 class NameCheckDialogFragment : DialogFragment() {
 
@@ -30,7 +33,7 @@ class NameCheckDialogFragment : DialogFragment() {
 //            val inflater = requireActivity().layoutInflater
 //            builder.setView(inflater.inflate(R.layout.activity_name_check_dialog, null))
             builder.setTitle("회원 이름 확인")
-                .setIcon(resources.getDrawable(R.drawable.main_logo, context!!.theme))
+//                .setIcon(resources.getDrawable(R.drawable.main_logo, context!!.theme))
                 .setMessage("문X훈 회원님 맞나요?")
                 .setPositiveButton("", DialogInterface.OnClickListener { dialog, _ ->
                     nameChecklistener.onDialogPositiveClick(this)
@@ -41,24 +44,27 @@ class NameCheckDialogFragment : DialogFragment() {
                 })
                 .setNegativeButtonIcon(resources.getDrawable(R.drawable.no_button_vector, context!!.theme))
             val dialog = builder.create()
+            dialog.setOnShowListener(OnShowListener { dialog ->
+//                val buttonHeight = resources.getDimensionPixelSize(R.dimen.no_button_width)
+//                val buttonWidth = resources.getDimensionPixelSize(R.dimen.yes_button_width)
 
-//            여기서부터 커스터마이징
-            //        Button Size Customzation
-//        https://stackoverflow.com/questions/34555107/how-to-customize-alertdialog-so-that-buttons-fit-in-the-alertdialog
-            val negativeButton: Button =
-                dialog.getButton(DialogInterface.BUTTON_NEGATIVE)
-            val positiveButton: Button =
-                dialog.getButton(DialogInterface.BUTTON_POSITIVE)
-            val params = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                2f
-            )
-            negativeButton.layoutParams = params
-            positiveButton.layoutParams = params
-
-            negativeButton.invalidate()
-            positiveButton.invalidate()
+                val negativeButton =
+                    (dialog as AlertDialog).getButton(DialogInterface.BUTTON_NEGATIVE)
+                val positiveButton =
+                    dialog.getButton(DialogInterface.BUTTON_POSITIVE)
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    1f
+                )
+                params.gravity = Gravity.CENTER
+                negativeButton.apply {
+                    layoutParams = params
+                }
+                positiveButton.apply {
+                    layoutParams = params
+                }
+            })
 
 //            커스터마이징 끝 return은 커스터마이징 된 dialog
             dialog
@@ -66,8 +72,16 @@ class NameCheckDialogFragment : DialogFragment() {
         } ?: throw IllegalStateException("Activity Can't be null")
     }
 
-    override fun onAttach(activity: Activity) {
-        super.onAttach(activity)
+    override fun onResume() {
+        val width = resources.getDimensionPixelSize(R.dimen.dialog_fragment_width)
+        val height = resources.getDimensionPixelSize(R.dimen.dialog_fragment_height)
+
+        dialog?.window?.setLayout(width, height)
+        super.onResume()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
         try {
             nameChecklistener = context as NameCheckListener
         } catch (e: ClassCastException) {

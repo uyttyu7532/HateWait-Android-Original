906e6c0ed4f79d52ac25ec8d3af9876afd523418
milkcoke <mbh023@konkuk.ac.kr>
2020-07-09 22:56:55 +0900

    "store name info update -> button activate"

--- app/src/main/java/com/example/hatewait/StoreInfoUpdate.kt
+++ app/src/main/java/com/example/hatewait/StoreInfoUpdate.kt
@@ -53,7 +53,8 @@ class StoreInfoUpdate : AppCompatActivity(),
         store_name.text = STORENAME
 //        DB로부터 불러온 변경 전 가게정보
         val initialStoreInfoMap : Map<String, String>
-                = mapOf(Pair("AUTO_CALL", auto_call_number.text.toString()),
+                = mapOf(Pair("NAME", store_name.text.toString()),
+                    Pair("AUTO_CALL", auto_call_number.text.toString()),
                     Pair("ADDRESS", store_address_editText.text.toString()),
                     Pair("CAPACITY", store_capacity_number_textView.text.toString()),
                     Pair("PHONE", store_phone_number_textView.text.toString()),
@@ -77,11 +78,13 @@ class StoreInfoUpdate : AppCompatActivity(),
         store_name_edit_button.setOnClickListener {
             StoreNameChangeDialog().show(supportFragmentManager, "STORE_NAME_CHANGE")
         }
+
         store_business_hours_text.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(updatedBusinessHours: Editable?) {
                 update_store_info_button.isEnabled =
                     initialStoreInfoMap["TIME"] != updatedBusinessHours.toString()
-                    || initialStoreInfoMap["AUTO_CALL"] != auto_call_number.toString()
+                    || initialStoreInfoMap["NAME"] != store_name.text.toString()
+                    || initialStoreInfoMap["AUTO_CALL"] != auto_call_number.text.toString()
                     || initialStoreInfoMap["ADDRESS"] != store_address_editText.text.toString()
                     || initialStoreInfoMap["CAPACITY"] != store_capacity_number_textView.text.toString()
                     || initialStoreInfoMap["PHONE"] != store_phone_number_textView.text.toString()
@@ -102,6 +105,7 @@ class StoreInfoUpdate : AppCompatActivity(),
 //                current Value (DB로부터 받아온 값)을 쥐고 비교해서 달라지면 버튼 활성화 (달라지는거 없으면 버튼바뀔이유 X)
                     update_store_info_button.isEnabled =
                         initialStoreInfoMap["AUTO_CALL"] != updatedCallNumber.toString()
+                        || initialStoreInfoMap["NAME"] != store_name.text.toString()
                         || initialStoreInfoMap["ADDRESS"] != store_address_editText.text.toString()
                         || initialStoreInfoMap["CAPACITY"] != store_capacity_number_textView.text.toString()
                         || initialStoreInfoMap["PHONE"] != store_phone_number_textView.text.toString()
@@ -120,7 +124,8 @@ class StoreInfoUpdate : AppCompatActivity(),
             override fun afterTextChanged(updatedCapacity: Editable?) {
                 update_store_info_button.isEnabled =
                     initialStoreInfoMap["CAPACITY"] != updatedCapacity.toString()
-                    || initialStoreInfoMap["AUTO_CALL"] != auto_call_number.toString()
+                    || initialStoreInfoMap["NAME"] != store_name.text.toString()
+                    || initialStoreInfoMap["AUTO_CALL"] != auto_call_number.text.toString()
                     || initialStoreInfoMap["ADDRESS"] != store_address_editText.text.toString()
                     || initialStoreInfoMap["PHONE"] != store_phone_number_textView.text.toString()
                     || initialStoreInfoMap["TIME"] != store_business_hours_text.text.toString()
@@ -140,7 +145,8 @@ class StoreInfoUpdate : AppCompatActivity(),
             override fun afterTextChanged(updatedPhoneNumber: Editable?) {
                 update_store_info_button.isEnabled =
                     initialStoreInfoMap["PHONE"] != updatedPhoneNumber.toString()
-                    || initialStoreInfoMap["AUTO_CALL"] != auto_call_number.toString()
+                    || initialStoreInfoMap["NAME"] != store_name.text.toString()
+                    || initialStoreInfoMap["AUTO_CALL"] != auto_call_number.text.toString()
                     || initialStoreInfoMap["CAPACITY"] != store_capacity_number_textView.text.toString()
                     || initialStoreInfoMap["ADDRESS"] != store_address_editText.text.toString()
                     || initialStoreInfoMap["TIME"] != store_business_hours_text.text.toString()

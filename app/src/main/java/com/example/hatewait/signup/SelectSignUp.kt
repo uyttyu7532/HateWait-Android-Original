e06c6d4dc60bda4eeadb0131fdf580b1fb34d43a
uyttyu7532 <uyttyu7532@naver.com>
2020-11-11 21:55:14 +0900

    백업

---
+++ app/src/main/java/com/example/hatewait/signup/SelectSignUp.kt
@@ -0,0 +1,12 @@
+package com.example.hatewait.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hatewait.R
+
+class SelectSignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_sign_up)
+    }
+}
